4a967ebf190c76fa06b41c1bff2b6905b630cfdd
Voy19 <37808736+Voy19@users.noreply.github.com>
2019-07-31 12:56:10 +0300

    fixed bug script

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,26 @@
 "use strict";
 
 window.onload = function () {
-  var head = document.querySelector('.paralax_img_head');
-  var smoke = document.querySelector('.paralax_img_smoke');
-  var text1 = document.querySelector('.paralax_img_text');
-  var text2 = document.querySelector('.paralax_img_text2');
-  var spray = document.querySelector('.paralax_img_spray');
-  window.addEventListener('scroll', function (e) {
-    var scroll = window.pageYOffset / 100;
-    var rate = scroll * 5;
-    var rate2 = scroll * 3;
-    var rate3 = scroll * 2;
-    var rate4 = scroll * 1;
-    head.style.transform = 'translate3d(0px, ' + Math.floor(rate2) + 'px, 0px)';
-    smoke.style.transform = 'translate3d(0px, -' + Math.floor(rate) + 'px, 0px)';
-    text1.style.transform = 'translate3d(0px, ' + Math.floor(rate3) + 'px, 0px)';
-    text2.style.transform = 'translate3d(0px, ' + Math.floor(rate4) + 'px, 0px)';
-    spray.style.transform = 'translate3d(0px, ' + Math.floor(rate) + 'px, 0px)';
-  });
+  if (document.querySelector('.paralax_img_head')) {
+    var head = document.querySelector('.paralax_img_head');
+    var smoke = document.querySelector('.paralax_img_smoke');
+    var text1 = document.querySelector('.paralax_img_text');
+    var text2 = document.querySelector('.paralax_img_text2');
+    var spray = document.querySelector('.paralax_img_spray');
+    window.addEventListener('scroll', function (e) {
+      var scroll = window.pageYOffset / 100;
+      var rate = scroll * 5;
+      var rate2 = scroll * 3;
+      var rate3 = scroll * 2;
+      var rate4 = scroll * 1;
+      head.style.transform = 'translate3d(0px, ' + Math.floor(rate2) + 'px, 0px)';
+      smoke.style.transform = 'translate3d(0px, -' + Math.floor(rate) + 'px, 0px)';
+      text1.style.transform = 'translate3d(0px, ' + Math.floor(rate3) + 'px, 0px)';
+      text2.style.transform = 'translate3d(0px, ' + Math.floor(rate4) + 'px, 0px)';
+      spray.style.transform = 'translate3d(0px, ' + Math.floor(rate) + 'px, 0px)';
+    });
+  }
+
   var iconClick = document.querySelector('.icon-click');
   iconClick.addEventListener('click', hideMenu);
 
